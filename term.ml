51749fe84c660e4f6dc50ec5c058c5f4e192320f
Emile-wa <emilethuillier@gmail.com>
2024-10-23 11:17:53 +0200

    Fin des questions de base !

--- term.ml
+++ term.ml
@@ -31,11 +31,19 @@ let fresh_var () = () |> fresh |> var
 
 let var_equals (v1 : var) (v2 : var) : bool = (v1 = v2)
 
-let list_fold_two_fun (f : 'a -> 'a -> 'b -> 'b) (l1 : 'a list) (l2 : 'a list) 
+let list_fold_two_left (f : 'a -> 'a -> 'b -> 'b) (l1 : 'a list) (l2 : 'a list) (b : 'b) : 'b = 
+	match l1,l2 with 
+	| t1::q1, t2::q2 -> f t1 t2 (list_fold_two_fun f q1 q2 b) 
+	| [],[] -> b
+	| _ -> failwith "les deux listes n'ont pas la même longueur !"
 
-let equals (t1 : var) (t2 : var) : bool = 
+
+let equals (t1 : t) (t2 : t) : bool = 
 	match t1,t2 with 
-	| Fun_t (s1, l1), Fun_t (s2,l2) when s1 = s2 ->
+	| Fun_t (s1, l1), Fun_t (s2,l2) when s1 = s2 -> 
+		list_fold_two_left (fun a b acc -> (equals a b) && acc) l1 l2 true
+	| Var_t v1, Var_t v2 -> var_equals v1 v2
+
 
 
 
